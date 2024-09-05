2c175be899d27641dd50f27639c3077305151780
Marcus Herrmann <mail@marcus-herrmann.com>
2024-09-05 16:34:11 +0200

    Refactor CSS

--- src/scripts.js
+++ src/scripts.js
@@ -84,7 +84,7 @@ if (document.querySelector("#produktliste")) {
 }
 
 
-toggleAriaExpanded(document.querySelector('.accountbutton'));
+toggleAriaExpanded(document.querySelector('.account--widget__button'));
 
 
 
